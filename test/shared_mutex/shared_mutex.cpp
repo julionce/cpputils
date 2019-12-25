2f4c92847105fb4c2beb5d6a3ff120b021441102
julianbermudez <julianbermudez@eprosima.com>
2019-12-25 11:17:47 +0100

    Move shared_mutex as an independent project.

--- test/shared_mutex/shared_mutex.cpp
+++ test/shared_mutex/shared_mutex.cpp
@@ -1,6 +1,24 @@
+/*
+ * Copyright 2019-present Julián Bermúdez Ortega.
+ *
+ * This file is part of VaneinsCpputils.
+ *
+ * VaneinsCpputils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VaneinsCpputils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VaneinsCpputils.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #define CATCH_CONFIG_MAIN
 
-#include <vaneins/utils/shared_mutex.hpp>
+#include <vaneins/util/shared_mutex/shared_mutex.hpp>
 
 #include <catch2/catch.hpp>
 
